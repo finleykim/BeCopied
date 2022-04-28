23984dcc2272d5bb9a6535c9474a2490b280378e
finleykim <g_kimmm@naver.com>
2022-04-28 18:36:38 +0900

    UI Configure Finished

---
+++ BeCopied/CollectionSelectViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionSelectViewController.swift
+//  BeCopied
+//
+//  Created by Finley on 2022/04/28.
+//
+
+import UIKit
+
+class CollectionSelectViewController: UIViewController{
+    
+}
---
+++ BeCopied/DetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewController.swift
+//  BeCopied
+//
+//  Created by Finley on 2022/04/28.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController{
+    
+}
